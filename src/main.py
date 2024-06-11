69be4a16cd0dd279a2031809f8e16c7aa89c4dc9
Aditya Wahyu Wijanarko <wijanarko.adityawahyu@gmail.com>
2024-06-11 14:12:53 +0700

    Adding stock database

--- src/main.py
+++ src/main.py
@@ -1,54 +1,110 @@
 import mylib
 
-print('Selamat Datang di Pasar Buah!')
+daftarBuah = [
+    [0, 'Apel', 20, 10000],
+    [1, 'Jeruk', 15, 15000],
+    [2, 'Anggur', 25, 20000]
+]
 
 
-stockApel = 10
-stockJeruk = 8
-stockAnggur = 15
+#s skema ke-1
+# daftarBuah = {
+#     'indeks': [0, 1, 2],
+#     'nama': ['Apel', 'Jeruk', 'Anggur'],
+#     'stock': [20, 15, 25],
+#     'harga'
+# }
 
-# Definisikan harga buah
-hargaApel = 10000
-hargaJeruk = 15000
-hargaAnggur = 20000
+# skema ke-2
+# daftarBuah = {
+#     'Apel': [0, 20, 10000],
+#     'Jeruk': [1, 15, 15000],
+#     'Anggur': [2, 20, 20000]
+# }
 
-# Minta input jumlah buah dan hitung harga buah
-nApel, totalHargaApel = mylib.inputBuah(nama='Apel', stock = stockApel, harga = hargaApel)
-nJeruk, totalHargaJeruk= mylib.inputBuah(nama='Jeruk', stock = stockJeruk, harga = hargaJeruk)
-nAnggur, totalHargaAnggur = mylib.inputBuah(nama='Anggur', stock = stockAnggur, harga = hargaAnggur)
+def main():
+# print('Selamat Datang di Pasar Buah!')
 
-
-# Hitung total harga belanja
-totalBelanja = totalHargaApel + totalHargaJeruk + totalHargaAnggur
-
-# Tampilkan rincian  belanja
-print(f'''
-Detail Belanja
+    listMenu = '''
       
-Apel: {nApel} x {hargaApel} = {totalHargaApel}
-Jeruk: {nJeruk} x {hargaJeruk} = {totalHargaJeruk}
-Anggur: {nAnggur} x {hargaAnggur} = {totalHargaAnggur}
+      Selamat Datang di Pasar Buah
 
-Total: {totalBelanja}
-''')
+      
+      List Menu:
+      1. Show
+      2. Add
+      3. Delete
+      4. Buy
+      5. Exit
+      '''
 
-# proses pembayaran
-while True:
-    # input jumlah uang
-    bayar = int(input('Silakan masukkan uang Anda: '))
+    while True:
+        print(listMenu)
+        option = input('Masukkan angka menu yang ingin dijalankan: ')
 
-    # hitung selisih antara bayar dengan total
-    selisih = totalBelanja - bayar
+        if option == '1':
+            mylib.show(daftarBuah)
+        elif option == '2':
+            mylib.add(daftarBuah)
+        elif option == '3':
+            mylib.delete(daftarBuah)
+        elif option == '4':
+            mylib.buy(daftarBuah)
+        elif option == '5':
+            break
+        else:
+            print(input('Input Anda salah. Silakan input ulang!'))
 
-    # bandingkan antara uang dengan total harga
-    if selisih > 0:
-        print(f'Uang Anda kurang sebesar Rp.{selisih}')
-        continue
 
-    # ucapan terima kasih
-    else:
-        print(f'''
-              Terima kasih!
-              Uang kembalian Anda : Rp.{abs(selisih)}''')
-        break
+main()
+
+
+# stockApel = 10
+# stockJeruk = 8
+# stockAnggur = 15
+
+# # Definisikan harga buah
+# hargaApel = 10000
+# hargaJeruk = 15000
+# hargaAnggur = 20000
+
+# # Minta input jumlah buah dan hitung harga buah
+# nApel, totalHargaApel = mylib.inputBuah(nama='Apel', stock = stockApel, harga = hargaApel)
+# nJeruk, totalHargaJeruk= mylib.inputBuah(nama='Jeruk', stock = stockJeruk, harga = hargaJeruk)
+# nAnggur, totalHargaAnggur = mylib.inputBuah(nama='Anggur', stock = stockAnggur, harga = hargaAnggur)
+
+
+# # Hitung total harga belanja
+# totalBelanja = totalHargaApel + totalHargaJeruk + totalHargaAnggur
+
+# # Tampilkan rincian  belanja
+# print(f'''
+# Detail Belanja
+      
+# Apel: {nApel} x {hargaApel} = {totalHargaApel}
+# Jeruk: {nJeruk} x {hargaJeruk} = {totalHargaJeruk}
+# Anggur: {nAnggur} x {hargaAnggur} = {totalHargaAnggur}
+
+# Total: {totalBelanja}
+# ''')
+
+# # proses pembayaran
+# while True:
+#     # input jumlah uang
+#     bayar = int(input('Silakan masukkan uang Anda: '))
+
+#     # hitung selisih antara bayar dengan total
+#     selisih = totalBelanja - bayar
+
+#     # bandingkan antara uang dengan total harga
+#     if selisih > 0:
+#         print(f'Uang Anda kurang sebesar Rp.{selisih}')
+#         continue
+
+#     # ucapan terima kasih
+#     else:
+#         print(f'''
+#               Terima kasih!
+#               Uang kembalian Anda : Rp.{abs(selisih)}''')
+#         break
 
--- src/mylib.py
+++ src/mylib.py
@@ -1,4 +1,182 @@
-def inputBuah(nama, stock, harga):
+from tabulate import tabulate
+
+def stringValidation(title):
+    """Fungsi untuk validasi tipe data string
+
+    Args:
+        title (String): Pesan yang akan ditampilkan pada layar
+
+    Returns:
+        String: Nilai yang diinputkan
+    """
+    while True:
+        teks = input(title)
+        if teks.isalpha() == True:
+            break
+        else:
+            print('Silakan input hanya teks')
+    return teks.capitalize()
+
+def integerValidation(title, minval = 0, maxval = 100):
+    """Fungsi untuk validasi bilangan bulat
+
+    Args:
+        title (String): Pesan yang akan ditampilkan pada layar
+        minval (int, optional): Nilai minimal. Defaults to 0.
+        maxval (int, optional): Nilai maksimal. Defaults to 100.
+
+    Returns:
+        Int: Nilai yang diinputkan
+    """
+    while True:
+        num = input(title)
+        try:
+            num = int(num)
+            if num >= minval and num <= maxval:
+                break
+            else:
+                print('Angka yang Anda masukkan di luar rentang.')
+        except:
+            print('Yang anda inputkan bukan bilangan')
+    return num
+
+def show(database, header = ['index', 'stock', 'name', 'price']):
+    print(tabulate(database, headers = header, tablefmt = 'grid'))
+
+def add(database):
+    name = stringValidation('Masukkan nama buah: ')
+    stock = integerValidation(
+        title = 'Masukkan stock buah: ',
+        minval = 0
+    )
+    price = integerValidation(
+        title = 'Masukkan harga buah: ',
+        minval = 0,
+        maxval = 100000
+    )
+
+    for id, buah in enumerate(database):
+        if name in buah:
+            database[id] = [id, name, stock, price]
+            break
+    else:
+        database.append([id + 1, name, stock, price])
+    
+    # menampilkan database terupdate
+    show(database)
+
+def delete(database):
+
+    show(database)
+
+    # meminta user input indeks yang akan dihapus
+    idx = integerValidation(
+        title = 'Masukkan index buah yang ingin dihapus: ',
+        maxval = len(database)
+    )
+
+    for id in range(len(database)):
+        if id == idx:
+            del database[idx]
+            break
+    else:
+        print('Buah yang Anda cari tidak tersedia')
+        
+        # Memperbarui index
+    for id, buah in enumerate(database):
+        if id != buah[0]:
+            database[id][0] = id
+
+        # menampilkan database terbaru
+    show(database)
+
+def buy(database):
+
+    databaseTemp = database.copy()
+    keranjang = []
+    reorder = None
+    
+    while reorder != 'No':
+
+        show(databaseTemp)
+
+          #meminta user input indeks dan jumlah buah yang ingin dibeli
+        id = integerValidation(
+            title = 'Silakan masukan index buah: ',
+            minval = 0,
+            maxval = len(database) - 1
+            )
+
+        stock = integerValidation(
+            title = 'Silakan masukkan jumlah buah: ',
+            minval = 0,
+            maxval = database[id][2]
+            )
+
+        # Menambahkan ke dalam keranjang belanja
+        keranjang.append([database[id][1], stock, databaseTemp[id][3]])
+        
+        # Menampilkan kernjang belanja updated
+        show(database = keranjang, header = ['Nama', 'Qty', 'Harga'])
+
+        # konfirmasi reorder
+        while True:
+            status = stringValidation('Mau beli yang lain? ').lower()
+            if status in ['yes', 'y', 'ya']:
+                reorder = 'Yes'
+            elif status in ['no', 'n', 'tidak']:
+                reorder = 'No'
+            break
+
+        databaseTemp[id][2] -= stock
+
+    # Menghitung total harga
+    total = 0
+    for id, item in enumerate(keranjang):
+        # Hitung total harga buah
+        totalHargaBuah = item[1] * item[2]
+        # Input total harga ke keranjang
+        keranjang[id].append(totalHargaBuah)
+        #sum seluruh harga
+        total += totalHargaBuah
+
+    # Menampilkan kernjang belanja updated
+    show(database = keranjang, header = ['Nama', 'Qty', 'Harga', 'Total Harga'])
+
+    # Menampilkan uang yang harus dibayar
+    print(f'Total yang harus Anda bayar adalah Rp.{total}')
+
+    # proses pembayaran
+    pembayaran(total)
+
+    # update database
+    database = databaseTemp.copy()
+
+def pembayaran(totalHarga):
+    while True:
+        # input jumlah uang
+        bayar = int(input('Silakan masukkan uang Anda: Rp. '))
+
+        # hitung selisih antara bayar dengan total
+        selisih = totalHarga - bayar
+
+        # bandingkan antara uang dengan total harga
+        if selisih > 0:
+            print(f'Uang Anda kurang sebesar Rp.{selisih}')
+            continue
+
+        # ucapan terima kasih
+        else:
+            print(f'''
+                Terima kasih!
+                Uang kembalian Anda : Rp.{abs(selisih)}''')
+            break
+    
+            
+
+
+
+# def inputBuah(nama, stock, harga):
     """Fungsi untuk meminta user input jumlah buah
     dan menghitung harganya
 
@@ -11,15 +189,3 @@ def inputBuah(nama, stock, harga):
         nBuah (Integer): Jumlah buah yang dipesan
         hargaBuah (Integer): Total harga buah
     """
-    while True:
-        nBuah = int(input(f'Masukkan jumlah {nama}: '))
-
-        if nBuah > stock:
-            print(f'Jumlah terlalu banyak, stock tersisa {stock} buah')
-            continue
-
-        break
-
-    hargaBuah = nBuah * harga
-
-    return nBuah, hargaBuah
